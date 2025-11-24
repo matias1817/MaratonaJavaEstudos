4ff5d7824d1a95efd7d35d890dc29b1be7ad32ba
matias <matias21.oliveirape@gmail.com>
2025-11-24 08:26:20 -0300

    operadores

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadore.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadore {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 20;
+
+        int resultado = numero1 * numero2;
+
+        System.out.println(numero1 + numero2);
+
+    }
+
+}
