565f9a5592a31f610716e29d36efcb1d51381e77
matias <matias21.oliveirape@gmail.com>
2026-01-16 07:55:16 -0300

    classes2

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/ProfessorTest.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Professor;
+
+public class ProfessorTest {
+    public static void main(String[] args) {
+        Professor professor = new Professor();
+        professor.nome = "Girafales";
+        professor.idade = 41;
+        professor.sexo = 'M';
+
+        System.out.println(professor.nome);
+        System.out.println(professor.idade);
+        System.out.println(professor.sexo);
+    }
+}
