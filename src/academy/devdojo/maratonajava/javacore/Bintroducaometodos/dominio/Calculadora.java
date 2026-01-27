f0dd1c194c77cf7b48507d48e72d60367c51376c
matias <matias21.oliveirape@gmail.com>
2026-01-27 08:31:51 -0300

    continuacao dos metodos

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -12,4 +12,30 @@ public class Calculadora {
     public void multiplicaDoisNumeros(int number1, int number2){
         System.out.println(number1 * number2);
     }
+
+    public double divideDoisNumeros(double number1, double number2){
+        if (number2 != 0){
+            return number1 / number2;
+        }
+        return 0;
+    }
+
+    public void imprimeDivisaoDeDoisNumeros(double number1, double number2){
+        if ( number2 == 0){
+            System.out.println("não existe divisão por 0");
+            return;
+        }
+        System.out.println(number1/number2);
+    }
+
+    public void alteraDoisNumeros (int number1, int number2){
+        number1 = 99;
+        number2 = 33;
+        System.out.println("Dentro");
+        System.out.println(number1);
+        System.out.println(number2);
+    }
+
+
+
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest04.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest04 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.alteraDoisNumeros(1,3);
+    }
+}
